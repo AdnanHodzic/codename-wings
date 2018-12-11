f77c64f2d5a2e085b686a83137930743ed6a5671
Adnan Hodzic <adnan@hodzic.org>
2018-12-11 11:00:42 +0100

    added missing vars

--- terraform-k8s-aws-eks-deploy/provider.tf
+++ terraform-k8s-aws-eks-deploy/provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region  = "eu-west-1"
+    access_key = "${var.AWS_ACCESS_KEY}"
+    secret_key = "${var.AWS_SECRET_KEY}"
+    region = "${var.AWS_REGION}"
 }
 
 data "aws_region" "current" {}
--- terraform-k8s-aws-eks-deploy/vars.tf
+++ terraform-k8s-aws-eks-deploy/vars.tf
@@ -1,3 +1,18 @@
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-02790d1ebf3b5181d"
+  }
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
 variable "cluster-name" {
   default = "terraform-k8s-eks-go-webapp"
   type    = "string"
